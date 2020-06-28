066f8e451164993343a28f7f10aa40153436f1ba
Jason Choo <choo.yansheng@gmail.com>
2020-06-28 18:33:34 +0800

    [C/Project] Refactored project

--- lib/ui/index.dart
+++ lib/ui/index.dart
@@ -1,10 +1,9 @@
 export 'screens/auth_screen.dart';
 export 'screens/camera_screen.dart';
 export 'screens/feed_screen.dart';
-
 export 'widgets/auth_guard.dart';
 export 'widgets/logo.dart';
 export 'widgets/none_scroll_behavior.dart';
-export 'widgets/outline_button.dart';
 export 'widgets/redirect.dart';
 export 'widgets/retro_body.dart';
+export 'widgets/retro_outline_button.dart';
